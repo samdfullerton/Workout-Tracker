fd21fd7a55b6f37e0cbbf710b46ef35ad3af55d0
Sam Fullerton <Samdfullerton@gmail.com>
2020-11-23 16:11:31 -0600

    updated mongodb password

--- routes/route-api.js
+++ routes/route-api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get("/api/workouts", (req, res) => { 
+ staurouter.get("/api/workouts", (req, res) => { 
     db.Workout.find({}, (err, data) => {
       if (err) {
           res.status(404).send("not working")
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://samfu:rootroot@cluster0.jmavo.mongodb.net/workouts?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
